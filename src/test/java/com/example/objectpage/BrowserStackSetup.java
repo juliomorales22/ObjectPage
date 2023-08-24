fa03853e324f9ee88a7a1832a3c1ddc1d9619d2c
“Julio <Julio_Morales@epam.com>
2023-08-24 10:57:51 +0200

    finish 3

--- src/test/java/com/example/objectpage/BrowserStackSetup.java
+++ src/test/java/com/example/objectpage/BrowserStackSetup.java
@@ -7,23 +7,28 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class BrowserStackSetup {
+
   String driverPath = "C:\\geckodriver.exe";
   WebDriver driver;
   BrowserStackHomePage objBrowserStackHomePage;
   BrowserStackSignUpPage objBrowserStackSignUpPage;
+
   @BeforeTest
   public void setup() {
-    System.setProperty("webdriver.chrome.driver", "C:\\Users\\Julio_Morales\\chromedriver\\chromedriver.exe");
+    System.setProperty("webdriver.chrome.driver",
+        "C:\\Users\\Julio_Morales\\chromedriver\\chromedriver.exe");
     driver = new ChromeDriver();
     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     driver.get("https://www.amazon.es/");
   }
+
   @Test(priority = 1)
   public void navigate_to_homepage_click_on_getstarted() {
     objBrowserStackHomePage = new BrowserStackHomePage(driver);
     objBrowserStackHomePage.veryStringOffer();
-   objBrowserStackHomePage.clickOnGetOffer();
+    objBrowserStackHomePage.clickOnGetOffer();
   }
+
   @Test(priority = 2)
   public void enter_userDetails() {
     objBrowserStackSignUpPage = new BrowserStackSignUpPage(driver);
--- src/test/java/com/example/objectpage/BrowserStackSignUpPage.java
+++ src/test/java/com/example/objectpage/BrowserStackSignUpPage.java
@@ -1,13 +1,14 @@
 package com.example.objectpage;
 
-import org.junit.jupiter.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import static org.assertj.core.api.Assertions.*;
 
 public class BrowserStackSignUpPage {
+
   WebDriver driver;
   @FindBy(xpath = "//a[contains(@data-nav-role,'signin')]")
   WebElement signInLink;
@@ -21,27 +22,34 @@ public class BrowserStackSignUpPage {
   WebElement password;
   @FindBy(xpath = "//span[contains(@id,'nav-link')]")
   WebElement textIamInside;
+
   public BrowserStackSignUpPage(WebDriver driver) {
     this.driver = driver;
     PageFactory.initElements(driver, this);
   }
+
   public void textIamInside() {
     String gettextIamInsideString = textIamInside.getText().trim();
-    assertThat(  gettextIamInsideString).isNotEmpty().contains("Hola Julio") ;//"Hola Julio",
+    assertThat(gettextIamInsideString).isNotEmpty().contains("Hola Julio");//"Hola Julio",
 
   }
+
   public void clickOnlinkLogging() {
-   signInLink.click();
+    signInLink.click();
   }
+
   public void clickOnbuttonToLogging1() {
     buttonToLogging1.click();
   }
+
   public void clickOnbuttonEntrance() {
     buttonEntrance.click();
   }
+
   public void enterBusinessEmail(String arg1) {
     businessEmail.sendKeys(arg1);
   }
+
   public void enterPasswrod(String arg1) {
     password.sendKeys(arg1);
   }
